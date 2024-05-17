930ae5b31c5fa1f0ff404d3729da27af1dbef5d7
niharikap123 <166387267+niharikap123@users.noreply.github.com>
2024-05-17 21:40:36 +0530

    Add files via upload

---
+++ PageObjectModelAprJrp/src/com/facebook/Pages/FriendsPage.java
@@ -0,0 +1,26 @@
+package com.facebook.Pages;
+
+import java.io.IOException;
+
+import com.facebook.page.MasterPage;
+
+public class FriendsPage extends MasterPage{
+	
+	//find a friend, unfollow, mutual friends etc
+	
+		public FriendsPage() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+		public void findFriends()
+		{
+			System.out.println(" fin a friends method");
+		}
+		
+		public void unfollow()
+		{
+			System.out.println(" unfollow method");
+		}
+
+}
---
+++ PageObjectModelAprJrp/src/com/facebook/Pages/LandingPage.java
@@ -0,0 +1,28 @@
+package com.facebook.Pages;
+
+import java.io.IOException;
+
+import com.facebook.page.MasterPage;
+
+public class LandingPage extends MasterPage{
+	
+	//like status,comment,post something etc
+	
+	
+		public LandingPage() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+		public void likeStatus()
+		{
+			System.out.println("like status method");
+		}
+		
+		public void comment()
+		{
+			
+			System.out.println("comment method");
+		}	
+
+}
